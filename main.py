b28b9c3ff3de8290070481fd3e2d036b6e1711d4
Luke Perry <lukezabala@gmail.com>
2024-04-26 15:23:20 +0800

    Activities for DICT Python Course

---
+++ main.py
@@ -0,0 +1,44 @@
+
+# Activity 1
+print("Hello World, what are you doing?")
+
+# Activity 2
+x = 7
+y = "cake"
+
+print(x)
+print(y)
+
+# Activity 3
+favorite_food = "pizza"
+friend_name = "Tina"
+favorite_drink = "soda"
+
+print("I like " + favorite_food + ".")
+print("I like eating " + favorite_food + " with my friend " + friend_name + ".")
+print("We like to drink " + favorite_drink + ", too.")
+
+# Activity 4
+character_job = "pirate"
+character_ride = "ship"
+character_souvenir = "gold"
+character_pet = "parrot"
+
+print("There was once a " + character_job + " who loved adventure.")
+print("The " + character_job + " would take her " + character_ride + " to unknown places.")
+print("She brings home a lot of " + character_souvenir + ".")
+print("Then she goes home to her pet " + character_pet + ", Chuckles")
+
+# Activity 5
+print("Roses are red, violets are blue.")
+
+if 10 > 7:
+    print("Ten is greater than seven!")
+if 16 < 42:
+    print("Sixteen is less than forty-two!")
+    print("A long time ago in a galaxy far, far away...")
+
+# Activity 6
+x = 7
+y = ("cake")
+print(x)
